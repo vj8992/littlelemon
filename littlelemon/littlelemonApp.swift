fa7781aa5336f2ca13754aa9101f8fa8b9eaecf5
vj <vm.8992@gmail.com>
2025-05-26 14:40:07 +0530

    Initial Commit

---
+++ littlelemon/littlelemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  littlelemonApp.swift
+//  littlelemon
+//
+//  Created by Vijay Jain on 26/05/25.
+//
+
+import SwiftUI
+
+@main
+struct littlelemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
